06ade58fb07151c725b296ebde8fc07c3ec7b33e
don <1318360+unyil@users.noreply.github.com>
2020-02-04 21:02:17 +0000

    test 1

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
